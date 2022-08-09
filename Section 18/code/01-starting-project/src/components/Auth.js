cd84989d78fc4bd42da8b101081921fff69ccdb5
Marta Prosniak-Kalwaj <manj@gft.com>
2022-08-09 12:26:00 +0200

    Refactoring

--- Section 18/code/01-starting-project/src/components/Auth.js
+++ Section 18/code/01-starting-project/src/components/Auth.js
@@ -1,18 +1,19 @@
 import classes from "./Auth.module.css";
 import { useDispatch } from "react-redux";
-import { authActions } from "../store";
+import { authActions } from "../store/auth";
 
 const Auth = () => {
   const dispatch = useDispatch();
 
-  const loginHandler = () => {
+  const loginHandler = (e) => {
+    e.preventDefault();
     dispatch(authActions.logIn());
   };
 
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={loginHandler}>
           <div className={classes.control}>
             <label htmlFor="email">Email</label>
             <input type="email" id="email" />
@@ -21,7 +22,7 @@ const Auth = () => {
             <label htmlFor="password">Password</label>
             <input type="password" id="password" />
           </div>
-          <button onClick={loginHandler}>Login</button>
+          <button type="submit">Login</button>
         </form>
       </section>
     </main>
--- Section 18/code/01-starting-project/src/store/index.js
+++ Section 18/code/01-starting-project/src/store/index.js
@@ -1,51 +1,12 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
-
-const initialCounterState = { counter: 0, counterIsVisible: true };
-
-// slice of a global state
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggle(state) {
-      state.counterIsVisible = !state.counterIsVisible;
-    },
-  },
-});
-
-const initialAuthState = { isAuthenticated: false };
-
-const authSlice = createSlice({
-  name: "authentication",
-  initialState: initialAuthState,
-  reducers: {
-    logIn(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+import { configureStore } from "@reduxjs/toolkit";
+import counterSliceReducer from "./counter";
+import authSliceReducer from "./auth";
 
 const store = configureStore({
   reducer: {
-    counter: counterSlice.reducer,
-    auth: authSlice.reducer,
+    counter: counterSliceReducer,
+    auth: authSliceReducer,
   },
 });
-
-// methods under counterActions will create actions to be dispatched
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
